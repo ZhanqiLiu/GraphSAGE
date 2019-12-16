eba97b55ef92efdb34a758790661aca69c26e0f9
radeliu <798279508@qq.com>
2019-12-16 16:03:23 +0800

    add bi-lstm layer for highway in layers.py and combine the inits.py file to utils.py

--- graphsage/layers.py
+++ graphsage/layers.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from graphsage.inits import zeros
+from graphsage.utils import zeros
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -72,8 +72,8 @@ class Layer(object):
 
 class Dense(Layer):
     """Dense layer."""
-    def __init__(self, input_dim, output_dim, dropout=0., 
-                 act=tf.nn.relu, placeholders=None, bias=True, featureless=False, 
+    def __init__(self, input_dim, output_dim, dropout=0.,
+                 act=tf.nn.relu, placeholders=None, bias=True, featureless=False,
                  sparse_inputs=False, **kwargs):
         super(Dense, self).__init__(**kwargs)
 
@@ -92,7 +92,7 @@ class Dense(Layer):
 
         with tf.variable_scope(self.name + '_vars'):
             self.vars['weights'] = tf.get_variable('weights', shape=(input_dim, output_dim),
-                                         dtype=tf.float32, 
+                                         dtype=tf.float32,
                                          initializer=tf.contrib.layers.xavier_initializer(),
                                          regularizer=tf.contrib.layers.l2_regularizer(FLAGS.weight_decay))
             if self.bias:
@@ -112,5 +112,46 @@ class Dense(Layer):
         # bias
         if self.bias:
             output += self.vars['bias']
-
         return self.act(output)
+
+class Bilstm(Layer):
+    """Dense layer."""
+    def __init__(self,feature_length,dropout=0.,**kwargs):
+        super(Bilstm, self).__init__(**kwargs)
+
+        self.dropout = dropout
+        self.feature_length = feature_length
+
+
+        if self.logging:
+            self._log_vars()
+
+    def _call(self, inputs):
+
+        X_initial = tf.stack(inputs, axis=0)
+        X = tf.reshape(X_initial , [-1, len(inputs), self.feature_length])
+
+        with tf.variable_scope(self.name + '_vars'):
+            lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(num_units=16, state_is_tuple=True)#tf.shape(Layer[1])[1]
+            lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=self.dropout)
+
+            lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(num_units=16, state_is_tuple=True)
+            lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=self.dropout)
+
+            outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, X, dtype=tf.float32)#[(?,3,16),(?,3,16)]
+
+            outputs_concat = tf.concat([outputs[0], outputs[1]], axis=2)#(?,3,32)
+            concatenated = tf.layers.dense(outputs_concat, units=1, use_bias=True, kernel_initializer = tf.contrib.layers.xavier_initializer(uniform=False))#(?,3,1)
+            #outputs_concat = tf.concat([outputs[0][-1], outputs[1][-1]], axis=1)  # (?,3,32)
+            #concatenated = tf.layers.dense(outputs_concat, units=3, use_bias=True, kernel_initializer = tf.contrib.layers.xavier_initializer(uniform=False))#(?,3,1)
+
+
+        #一个w，有多少条数据都可以进来，最后对每个样本点都生成一个3维向量，不能做平均
+        bacth_average = tf.math.reduce_mean(tf.reshape(concatenated, [-1, len(inputs)]),axis=0)
+
+        # 可以选择：concatenated，bacth_average
+        weight_each_vertex = bacth_average
+
+        s = tf.nn.softmax(weight_each_vertex)
+
+        return s
