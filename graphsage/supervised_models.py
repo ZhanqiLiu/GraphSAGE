2d1b15098ecfd43cfe1fae1ff1f82d8af20cecde
radeliu <798279508@qq.com>
2019-12-16 17:15:32 +0800

    transfer the feature to placeholder, add the auc computation and the configure of aggregate_lstm is supported.

--- graphsage/supervised_models.py
+++ graphsage/supervised_models.py
@@ -1,27 +1,28 @@
 import tensorflow as tf
-
+from sklearn import metrics
 import graphsage.models as models
 import graphsage.layers as layers
-from graphsage.aggregators import MeanAggregator, MaxPoolingAggregator, MeanPoolingAggregator, SeqAggregator, GCNAggregator
+from graphsage.aggregators import MeanAggregator, GCNAggregator, GatAggregator
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
+
 
 class SupervisedGraphsage(models.SampleAndAggregate):
     """Implementation of supervised GraphSAGE."""
 
     def __init__(self, num_classes,
-            placeholders, features, adj, degrees,
-            layer_infos, concat=True, aggregator_type="mean", 
-            model_size="small", sigmoid_loss=False, identity_dim=0,
-                **kwargs):
+                 placeholders, features, adj, degrees,
+                 layer_infos, feature_shape, concat=False, aggregator_type="mean",
+                 model_size="small", sigmoid_loss=False, identity_dim=0,
+                 **kwargs):
         '''
         Args:
             - placeholders: Stanford TensorFlow placeholder object.
             - features: Numpy array with node features.
             - adj: Numpy array with adjacency lists (padded with random re-samples)
-            - degrees: Numpy array with node degrees. 
-            - layer_infos: List of SAGEInfo namedtuples that describe the parameters of all 
+            - degrees: Numpy array with node degrees.
+            - layer_infos: List of SAGEInfo namedtuples that describe the parameters of all
                    the recursive layers. See SAGEInfo definition above.
             - concat: whether to concatenate during recursive iterations
             - aggregator_type: how to aggregate neighbor information
@@ -33,38 +34,39 @@ class SupervisedGraphsage(models.SampleAndAggregate):
 
         if aggregator_type == "mean":
             self.aggregator_cls = MeanAggregator
-        elif aggregator_type == "seq":
-            self.aggregator_cls = SeqAggregator
-        elif aggregator_type == "meanpool":
-            self.aggregator_cls = MeanPoolingAggregator
-        elif aggregator_type == "maxpool":
-            self.aggregator_cls = MaxPoolingAggregator
         elif aggregator_type == "gcn":
             self.aggregator_cls = GCNAggregator
+        elif aggregator_type == "gat":
+            self.aggregator_cls = GatAggregator
         else:
             raise Exception("Unknown aggregator: ", self.aggregator_cls)
 
         # get info from placeholders...
-        self.inputs1 = placeholders["batch"]
+        self.inputs1 = placeholders["batch"]  # inputs1是batch的index
         self.model_size = model_size
         self.adj_info = adj
         if identity_dim > 0:
-           self.embeds = tf.get_variable("node_embeddings", [adj.get_shape().as_list()[0], identity_dim])
+            self.embeds = tf.get_variable("node_embeddings", [adj.get_shape().as_list()[0], identity_dim])
         else:
-           self.embeds = None
-        if features is None: 
+            self.embeds = None
+        if features is None:
             if identity_dim == 0:
                 raise Exception("Must have a positive value for identity feature dimension if no input features given.")
             self.features = self.embeds
         else:
-            self.features = tf.Variable(tf.constant(features, dtype=tf.float32), trainable=False)
+            # self.features = tf.Variable(tf.constant(features, dtype=tf.float32), trainable=False)
+            self.features = features
             if not self.embeds is None:
                 self.features = tf.concat([self.embeds, self.features], axis=1)
         self.degrees = degrees
         self.concat = concat
+        self.dropout = 0.1
+
         self.num_classes = num_classes
         self.sigmoid_loss = sigmoid_loss
-        self.dims = [(0 if features is None else features.shape[1]) + identity_dim]
+        # self.dims = [(0 if features is None else features.shape[1]) + identity_dim]
+        self.dims = [(0 if features is None else feature_shape) + identity_dim]
+
         self.dims.extend([layer_infos[i].output_dim for i in range(len(layer_infos))])
         self.batch_size = placeholders["batch_size"]
         self.placeholders = placeholders
@@ -74,27 +76,37 @@ class SupervisedGraphsage(models.SampleAndAggregate):
 
         self.build()
 
-
     def build(self):
+        # support_sizes1=[1, 10, 250],sample返回bacth的三层关联节点
         samples1, support_sizes1 = self.sample(self.inputs1, self.layer_infos)
         num_samples = [layer_info.num_samples for layer_info in self.layer_infos]
-        self.outputs1, self.aggregators = self.aggregate(samples1, [self.features], self.dims, num_samples,
-                support_sizes1, concat=self.concat, model_size=self.model_size)
+
+        if self.aggregator_cls == GatAggregator:
+            self.outputs1, self.aggregators = self.aggregate_lstm(samples1, [self.features], self.dims, num_samples,
+                                                                  support_sizes1, concat=self.concat,
+                                                                  model_size=self.model_size)
+        else:
+            self.outputs1, self.aggregators = self.aggregate(samples1, [self.features], self.dims, num_samples,
+                                                             support_sizes1, concat=self.concat,
+                                                             model_size=self.model_size)
+
         dim_mult = 2 if self.concat else 1
 
         self.outputs1 = tf.nn.l2_normalize(self.outputs1, 1)
 
         dim_mult = 2 if self.concat else 1
-        self.node_pred = layers.Dense(dim_mult*self.dims[-1], self.num_classes, 
-                dropout=self.placeholders['dropout'],
-                act=lambda x : x)
+        self.node_pred = layers.Dense(dim_mult * self.dims[-1], self.num_classes,
+                                      dropout=self.placeholders['dropout'],
+                                      act=lambda x: x)
         # TF graph management
         self.node_preds = self.node_pred(self.outputs1)
 
         self._loss()
+        self._auc()
+
         grads_and_vars = self.optimizer.compute_gradients(self.loss)
-        clipped_grads_and_vars = [(tf.clip_by_value(grad, -5.0, 5.0) if grad is not None else None, var) 
-                for grad, var in grads_and_vars]
+        clipped_grads_and_vars = [(tf.clip_by_value(grad, -5.0, 5.0) if grad is not None else None, var)
+                                  for grad, var in grads_and_vars]
         self.grad, _ = clipped_grads_and_vars[0]
         self.opt_op = self.optimizer.apply_gradients(clipped_grads_and_vars)
         self.preds = self.predict()
@@ -106,18 +118,27 @@ class SupervisedGraphsage(models.SampleAndAggregate):
                 self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
         for var in self.node_pred.vars.values():
             self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
-       
+
         # classification loss
         if self.sigmoid_loss:
             self.loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-                    logits=self.node_preds,
-                    labels=self.placeholders['labels']))
+                logits=self.node_preds,
+                labels=self.placeholders['labels']))
         else:
             self.loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-                    logits=self.node_preds,
-                    labels=self.placeholders['labels']))
+                logits=self.node_preds,
+                labels=self.placeholders['labels']))
 
         tf.summary.scalar('loss', self.loss)
+
+    def _auc(self):
+
+        self.auc_value, self.auc_op = tf.metrics.auc(tf.math.argmax(self.placeholders['labels'], axis=1),
+                                                     tf.math.argmax(self.node_preds, axis=1))
+        print("auc_truelabel:", tf.math.argmax(self.placeholders['labels'], axis=1))
+        print("auc_predictlabel:", tf.math.argmax(self.node_preds, axis=1))
+
+        tf.summary.scalar('auc_value', self.auc_value)
 
     def predict(self):
         if self.sigmoid_loss:
